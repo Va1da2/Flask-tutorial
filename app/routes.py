228761a684c682f0dacb205ed34360c16ce6c48f
Vaidas <vaidas.armonas@gmail.com>
2018-01-15 21:30:24 +0200

    Second lesson

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
 from app import app 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {"username": "Vaidas"}
+    posts = [
+        {
+            "author": {"username": "John"},
+            "body": "Beautiful day in Vilnius!"
+        },
+        {
+            "author": {"username": "Susan"},
+            "body": "Dragon Ball Super rocks!"
+        }
+    ]
+
+    return render_template('index.html', title="Home", user=user, posts=posts)
